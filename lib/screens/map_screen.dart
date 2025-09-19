b4f84e17eafc1226827b970a6550d3c06188c363
SOHAM-THUMMAR <sohamthummar04@gmail.com>
2025-09-19 13:28:07 +0530

    working

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:get/get.dart';
+import 'package:get/get.dart'; 
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:geolocator/geolocator.dart';
+
+
 import '../widgets/bottom_navigation_bar.dart';
 import 'submit_crowd_level_screen.dart';
 import '../core/app_theme.dart';
